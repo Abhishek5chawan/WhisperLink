11e61d35fac524e49cf57c608aad9bf65120299f
Abhishek5Chawan <abhishekchawan907@gmail.com>
2024-10-06 08:06:16 -0700

    test

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -68,3 +68,5 @@ const UserSchema: mongoose.Schema<User> = new mongoose.Schema({
 const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
 
 export default UserModel
+
+//hello world
